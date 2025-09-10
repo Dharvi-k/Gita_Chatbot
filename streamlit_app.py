d2fb4d670f0f4f354fbb332c6024f7c56488352a
Dharvi <kdharvi01@gmail.com>
2025-09-10 14:12:33 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ HF_API_KEY = st.secrets["HF_API_KEY"]  # Add in Streamlit -> Settings -> Secrets
 HEADERS = {"Authorization": f"Bearer {HF_API_KEY}"}
 
 # Free-tier friendly model
-MODEL = "gpt2-medium"
+MODEL = "facebook/bart-large"
 
 # ==========================
 # Query Hugging Face Model
@@ -73,6 +73,7 @@ if st.button("Ask"):
         st.success(answer)
     else:
         st.warning("Please enter a question.")
+
 
 
 
